1503129ae4575a1bcb25e6fcc60093f1074330e2
root <juanandreskoch@gmail.com>
2025-03-11 02:51:18 -0400

    Conexion a la base de datos

--- aplicacion/app.py
+++ aplicacion/app.py
@@ -1,11 +1,47 @@
 from flask import Flask
 from flask_login import LoginManager
+import pymysql
+from os import getenv
+from dotenv import load_dotenv
+from flask_bcrypt import Bcrypt, generate_password_hash
 
 def create_app():
     app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path='/')
 
+    app.config['SECRET_KEY'] = getenv('SECRET_KEY')
+    app.config['DB_HOST'] = getenv('DB_HOST')
+    app.config['DB_USER'] = getenv('DB_USER')
+    app.config['DB_PASSWORD'] = getenv('DB_PASSWORD')
+    app.config['DB_NAME'] = getenv('DB_NAME')
+
+    app.secret_key = app.config['SECRET_KEY']
+
+    db = pymysql.connect(
+    host=app.config['DB_HOST'],
+    port=3306,
+    user=app.config['DB_USER'],
+    password=app.config['DB_PASSWORD'],
+    database=app.config['DB_NAME']
+)
+
+    try:
+        with db.cursor() as cursor:
+            # Ejecutar una consulta sencilla para comprobar la conexión
+            cursor.execute("SELECT 1")
+            result = cursor.fetchone()
+            if result:
+                print("Conexion")
+            else:
+                print("error")
+    except pymysql.Error as e:
+        print(f"Error al conectar a la base de datos: {e}")
+    
+    app.app_context().push()
+
+
     login_manager = LoginManager()
     login_manager.init_app(app)
+    bcrypt = Bcrypt(app)
 
 
     @login_manager.user_loader
@@ -15,5 +51,9 @@ def create_app():
     # importar blueprints
     from aplicacion.blueprints.usuarios.routes import usuario
     app.register_blueprint(usuario, url_prefix='/')
+
+    # Pasar la conexión a la base de datos al Blueprint
+    app.config['db'] = db
+
 
     return app
--- aplicacion/blueprints/usuarios/routes.py
+++ aplicacion/blueprints/usuarios/routes.py
@@ -1,4 +1,4 @@
-from flask import request, render_template, redirect, url_for, Blueprint
+from flask import request, render_template, redirect, url_for, Blueprint, current_app
 from flask_login import login_user, logout_user, current_user
 from flask_bcrypt import Bcrypt, generate_password_hash
 
@@ -7,4 +7,18 @@ bcrypt = Bcrypt()
 
 @usuario.route('/')
 def index():
-    return render_template('usuarios/index.html')
+    db = current_app.config['db']
+    try:
+        cur = db.cursor()
+        cur.execute('SELECT u.nombre, u.apellido FROM alumnos a JOIN usuarios u ON a.idusuarios = u.idusuarios')
+        alumnos = cur.fetchall()
+        insertalumnos = []
+        columNamnes = [column[0] for column in cur.description]
+        for record in alumnos:
+            insertalumnos.append(dict(zip(columNamnes, record)))
+    except Exception as e:
+        print(e)
+    finally:
+        cur.close()
+
+    return render_template('usuarios/index.html', alumnos = insertalumnos)
