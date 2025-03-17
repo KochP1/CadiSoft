5e671a438e8f5e41dc2d8af4f1bcb7658de5790a
root <juanandreskoch@gmail.com>
2025-03-17 11:48:01 -0400

    Subiendo imagenes

--- aplicacion/blueprints/usuarios/routes.py
+++ aplicacion/blueprints/usuarios/routes.py
@@ -5,7 +5,7 @@ from flask_bcrypt import Bcrypt, generate_password_hash
 usuario = Blueprint('usuario', __name__, template_folder='templates', static_folder="static")
 bcrypt = Bcrypt()
 
-@usuario.route('/')
+@usuario.route('/', methods = ['GET', 'POST'])
 def index():
     db = current_app.config['db']
     try:
