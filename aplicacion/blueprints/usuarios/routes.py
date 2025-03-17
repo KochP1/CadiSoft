17da9961b4b0f700816012d35743af8d347940ec
root <juanandreskoch@gmail.com>
2025-03-17 11:50:18 -0400

    Empezando development

--- aplicacion/blueprints/usuarios/routes.py
+++ aplicacion/blueprints/usuarios/routes.py
@@ -5,20 +5,6 @@ from flask_bcrypt import Bcrypt, generate_password_hash
 usuario = Blueprint('usuario', __name__, template_folder='templates', static_folder="static")
 bcrypt = Bcrypt()
 
-@usuario.route('/')
+@usuario.route('/', methods = ['GET', 'POST'])
 def index():
-    db = current_app.config['db']
-    try:
-        cur = db.cursor()
-        cur.execute('SELECT u.nombre, u.apellido FROM alumnos a JOIN usuarios u ON a.idusuarios = u.idusuarios')
-        alumnos = cur.fetchall()
-        insertalumnos = []
-        columNamnes = [column[0] for column in cur.description]
-        for record in alumnos:
-            insertalumnos.append(dict(zip(columNamnes, record)))
-    except Exception as e:
-        print(e)
-    finally:
-        cur.close()
-
-    return render_template('usuarios/index.html', alumnos = insertalumnos)
+    return render_template('usuarios/index.html')
