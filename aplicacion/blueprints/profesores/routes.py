5de9d51e1ba14afbaf5321dc786b9cbe98014e8f
kochp1 <juanandreskochp@gmail.com>
2025-04-14 18:00:42 -0400

    Añadir soporte para solicitudes POST en la ruta de profesores; validar cuerpo JSON y campos requeridos.

--- aplicacion/blueprints/profesores/routes.py
+++ aplicacion/blueprints/profesores/routes.py
@@ -5,10 +5,29 @@ from flask_bcrypt import Bcrypt
 profesores = Blueprint('profesores', __name__, template_folder='templates', static_folder="static")
 bcrypt = Bcrypt()
 
-@profesores.route('/', methods = ['GET'])
+@profesores.route('/', methods = ['GET', 'POST'])
 def index():
     db = current_app.config['db']
     cur = db.cursor()
+    if request.method == 'POST':
+        if not request.is_json:
+            return jsonify({"error": "El cuerpo debe ser JSON"}), 400
+            
+        data = request.get_json()
+
+        required_fields = ['']
+
+        if not all(field in data for field in required_fields):
+            return jsonify({'error', 'faltan campos'}), 400
+        
+        try:
+            pass
+
+        except Exception as e:
+            print(e)
+            return redirect(url_for('profesores.index'))
+        finally:
+            cur.close()
     try:
         sql = 'SELECT p.idProfesor, p.idusuarios, u.nombre, u.segundoNombre, u.apellido, u.SegundoApellido, u.cedula, u.email, e.especialidad FROM profesores p JOIN usuarios u ON p.idusuarios = u.idusuarios JOIN especialidad e ON p.idespecialidad = e.idespecialidad'
         cur.execute(sql)
