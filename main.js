5694c77dd03fb43b01dfe7b5961cd39baeddc243
kochp1 <juanandreskochp@gmail.com>
2025-04-14 17:45:01 -0400

    Refactor main.js to simplify window creation and enhance URL loading; update window dimensions.

--- main.js
+++ main.js
@@ -1,63 +1,21 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow } = require('electron')
 const path = require('path');
-const { exec } = require('child_process');
+// Habilitar la carga de URL externas
+app.commandLine.appendSwitch('disable-web-security');
+app.commandLine.appendSwitch('ignore-certificate-errors');
 
-let mainWindow;
+const createWindow =  () => {
+    const window = new BrowserWindow({
+        width: 1200,
+        height: 720
+    })
 
-function createWindow() {
-    // Crear la ventana principal
-    mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false,
-        },
-    });
-
-    // Cargar la aplicación Flask
-    mainWindow.setMenuBarVisibility(false); // Esto ocultará la barra de menú
-    mainWindow.loadURL('http://localhost:5000');  // Cambia el puerto si es necesario
-
-    // Abrir las herramientas de desarrollo (opcional)
-    // mainWindow.webContents.openDevTools();
-
-    // Manejar el cierre de la ventana
-    mainWindow.on('closed', () => {
-        mainWindow = null;
-    });
+    window.setMenuBarVisibility(false);
+    window.loadURL('http://127.0.0.1:5000/')
 }
 
-// Iniciar la aplicación Flask cuando Electron esté listo
-app.on('ready', () => {
-    // Ejecutar el servidor Flask
-    exec('python3 run.py', (error, stdout, stderr) => {
-        if (error) {
-            console.error(`Error al iniciar Flask: ${error.message}`);
-            return;
-        }
-        if (stderr) {
-            console.error(`Error en Flask: ${stderr}`);
-            return;
-        }
-        console.log(`Flask iniciado: ${stdout}`);
-    });
-
-    // Crear la ventana de Electron
-    createWindow();
-});
-
-// Salir cuando todas las ventanas estén cerradas
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
-});
-
-app.on('activate', () => {
-    if (mainWindow === null) {
-        createWindow();
-    }
-});
+app.whenReady().then(() => {
+    createWindow()
+  })
 
 // npm install electron --save-dev
