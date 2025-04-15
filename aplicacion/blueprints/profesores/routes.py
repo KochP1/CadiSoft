06eb70be2f2df7c30d0f8e7120e06b14f3cb2e1c
kochp1 <juanandreskochp@gmail.com>
2025-04-15 19:12:26 -0400

    Añadir funcionalidad para crear profesores; implementar validación de campos y manejo de errores en la ruta de profesores.

--- aplicacion/blueprints/profesores/routes.py
+++ aplicacion/blueprints/profesores/routes.py
@@ -9,25 +9,50 @@ bcrypt = Bcrypt()
 def index():
     db = current_app.config['db']
     cur = db.cursor()
+
+    # POST profesor
     if request.method == 'POST':
         if not request.is_json:
             return jsonify({"error": "El cuerpo debe ser JSON"}), 400
             
         data = request.get_json()
 
-        required_fields = ['']
+        required_fields = ['nombre', 'segundoNombre', 'apellido', 'segundoApellido', 'cedula', 'email', 'contraseña', 'rol', 'especialidad']
 
         if not all(field in data for field in required_fields):
-            return jsonify({'error', 'faltan campos'}), 400
+            return jsonify({'error': 'faltan campos'}), 400
         
         try:
-            pass
+            sql_usuario = 'INSERT INTO usuarios (`nombre`, `segundoNombre`, `apellido`, `segundoApellido`, `cedula`, `email`, `contraseña`, `rol`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)'
+            contraseña_hash = bcrypt.generate_password_hash(data['contraseña']).decode('utf-8')
+            usuario = (
+                data['nombre'], 
+                data['segundoNombre'], 
+                data['apellido'],
+                data['segundoApellido'],
+                data['cedula'],
+                data['email'],
+                contraseña_hash,
+                data['rol']
+                )
+            cur.execute(sql_usuario, usuario)
+            db.commit()
 
+            cur.execute('SELECT idusuarios FROM usuarios WHERE cedula = %s', (data['cedula'],))
+            idusuario = cur.fetchone()
+
+            sql_profesor = 'INSERT INTO profesores (`idusuarios`, `idespecialidad`) VALUES (%s, %s)'
+            profesor = (idusuario, data['especialidad'])
+            cur.execute(sql_profesor, profesor)
+            db.commit()
+            return jsonify({'mensaje': 'profesor creado satisfactiramente'}), 200
         except Exception as e:
             print(e)
-            return redirect(url_for('profesores.index'))
+            return jsonify({'error': 'Error al crear el usuario'}), 400
         finally:
             cur.close()
+
+    # GET PROFESORES
     try:
         sql = 'SELECT p.idProfesor, p.idusuarios, u.nombre, u.segundoNombre, u.apellido, u.SegundoApellido, u.cedula, u.email, e.especialidad FROM profesores p JOIN usuarios u ON p.idusuarios = u.idusuarios JOIN especialidad e ON p.idespecialidad = e.idespecialidad'
         cur.execute(sql)
@@ -36,8 +61,19 @@ def index():
         columNames = [column[0] for column in cur.description]
         for record in registros:
             InsertRegistros.append(dict(zip(columNames, record)))
-        print(InsertRegistros)
-        return render_template('profesores/index.html', profesores = InsertRegistros)
+        cur.close()
+
+        cur = db.cursor()
+        sql_especialidades = 'SELECT * FROM especialidad'
+        cur.execute(sql_especialidades)
+        especialidad = cur.fetchall()
+        insertEspecialidad = []
+        columNamesEspecialidad = [column[0] for column in cur.description]
+        for record in especialidad:
+            insertEspecialidad.append(dict(zip(columNamesEspecialidad, record)))
+        print(insertEspecialidad)
+
+        return render_template('profesores/index.html', profesores = InsertRegistros, especialidades = insertEspecialidad)
     except Exception as e:
         print(e)
         return render_template('profesores/index.html')
--- aplicacion/static/js/script.js
+++ aplicacion/static/js/script.js
@@ -14,3 +14,54 @@ async function log_out() {
         alert('Error al cerrar sesión')
     }
 }
+
+// Crear profesor
+
+function crearProfesor() {
+    const form = document.getElementById('crear-profesor-form');
+    const rol = 'profesor'
+    const nombre = document.getElementById('nombreProfesor').value;
+    const segundoNombre = document.getElementById('segundoNombreProfesor').value;
+    const apellido = document.getElementById('apellidoProfesor').value;
+    const segundoApellido = document.getElementById('segundoApellidoProfesor').value;
+    const cedula = document.getElementById('cedulaProfesor').value;
+    const email = document.getElementById('emailProfesor').value;
+    const contraseña = document.getElementById('contraseñaProfesor').value;
+    const especialidad = document.getElementById('departamentoProfesor').value;
+
+    if (especialidad == '') {
+        alert('Debe escoger una especialidad')
+    }
+    form.addEventListener('submit', async (event) => {
+        event.preventDefault();
+
+        try {
+            const response = await fetch('/profesores/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    nombre: nombre,
+                    segundoNombre: segundoNombre,
+                    apellido: apellido,
+                    segundoApellido: segundoApellido,
+                    cedula: cedula,
+                    email: email,
+                    contraseña: contraseña,
+                    rol: rol,
+                    especialidad: especialidad
+                })
+            });
+    
+            if (response.ok) {
+                alert('Profesor creado satisfactoriamente')
+                window.location.href = '/profesores/'
+            } else {
+                alert('Error al crear el usuario')
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    })
+}
