0cc9e0be9f4175eb05dfb8ec47535b68403238d3
kochp1 <juanandreskochp@gmail.com>
2025-04-21 16:12:51 -0400

    Implementar funcionalidad para migrar datos a Excel; actualizar botones en las plantillas y agregar lógica de exportación en el script.

---
+++ aplicacion/static/js/excel.js
@@ -0,0 +1,32 @@
+function excel() {
+    const reportes = document.getElementById('tablaAlumnos');
+    let tableExport = new TableExport(reportes, {
+        exportButtons: false,
+        filename: "Prueba",
+        sheetname: "Tabla",
+    });
+
+    let datos = tableExport.getExportData();
+    let pref = datos.tablaAlumnos.xlsx;
+    tableExport.export2file(
+            pref.data,
+            pref.mimetype,
+            pref.filename,
+            pref.fileExtension,
+            pref.merges,
+            pref.RTL,
+            pref.sheetname
+        );
+    window.location.reload();
+}
+
+function migrarExcel() {
+
+    const btnsTabla = document.querySelectorAll('.tabla-btn')
+
+    for (let i = 0; i < btnsTabla.length; i++) {
+        btnsTabla[i].remove();
+    }
+
+    excel()
+}
