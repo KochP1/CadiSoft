dd8f78299e06192e14b4ecc4bb65403c0c6230bd
kochp1 <juanandreskochp@gmail.com>
2025-06-02 11:02:10 -0400

    Implementar la visualización de cursos en la plantilla y agregar funcionalidad para crear, buscar y eliminar cursos.

--- aplicacion/blueprints/cursos/routes.py
+++ aplicacion/blueprints/cursos/routes.py
@@ -4,3 +4,22 @@ from flask_bcrypt import Bcrypt, generate_password_hash
 
 cursos = Blueprint('cursos', __name__, template_folder='templates', static_folder="static")
 bcrypt = Bcrypt()
+
+@cursos.route('/', methods = ['GET', 'POST'])
+def index():
+    db = current_app.config['db']
+
+    with db.cursor() as cur:
+        try:
+            db.ping(reconnect=True)
+            sql = 'SELECT c.idCurso, f.idFacultad, f.facultad, c.nombre_curso FROM cursos c JOIN facultades f ON c.idFacultad = f.idFacultad'
+            cur.execute(sql)
+            registros = cur.fetchall()
+            insertCursos = []
+            columNames = [column[0] for column in cur.description]
+            for record in registros:
+                insertCursos.append(dict(zip(columNames, record)))
+            print(insertCursos)
+            return render_template('cursos/index.html', cursos = insertCursos)
+        except Exception as e:
+            print(e)
