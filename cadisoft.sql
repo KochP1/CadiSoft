8f1d96471745c54108811ef4d9146fe6ef06d408
kochp1 <juanandreskochp@gmail.com>
2025-06-26 18:04:52 -0400

    Actualizar tiempo de generación en el volcado SQL

--- cadisoft.sql
+++ cadisoft.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 14-06-2025 a las 00:28:52
+-- Tiempo de generación: 27-06-2025 a las 00:03:09
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
