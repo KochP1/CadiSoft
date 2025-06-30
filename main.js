e6954c9dff2c741e7ae62de7e7e3b54ed9ec9671
kochp1 <juanandreskochp@gmail.com>
2025-06-30 14:43:33 -0400

    Agregar soporte para atajos de teclado globales para navegación hacia atrás y hacia adelante en la ventana del navegador

--- main.js
+++ main.js
@@ -1,21 +1,38 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('path');
-// Habilitar la carga de URL externas
-app.commandLine.appendSwitch('disable-web-security');
-app.commandLine.appendSwitch('ignore-certificate-errors');
+const { app, BrowserWindow, globalShortcut } = require('electron')
+const path = require('path')
 
-const createWindow =  () => {
+const createWindow = () => {
     const window = new BrowserWindow({
         width: 1200,
-        height: 720
+        height: 720,
+        webPreferences: {
+            nodeIntegration: true
+        }
     })
 
-    window.setMenuBarVisibility(false);
+    window.setMenuBarVisibility(false)
     window.loadURL('http://127.0.0.1:5000/')
+
+    // Atajos de teclado globales
+    globalShortcut.register('Alt+Left', () => {
+        if (window.webContents.canGoBack()) {
+            window.webContents.goBack()
+        }
+    })
+
+    globalShortcut.register('Alt+Right', () => {
+        if (window.webContents.canGoForward()) {
+            window.webContents.goForward()
+        }
+    })
 }
 
 app.whenReady().then(() => {
     createWindow()
-  })
+})
 
+// Limpiar atajos al cerrar
+app.on('will-quit', () => {
+    globalShortcut.unregisterAll()
+})
 // npm install electron --save-dev
