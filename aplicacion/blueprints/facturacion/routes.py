f410e126489af56009c9b2b11d379dded93e4a67
kochp1 <juanandreskochp@gmail.com>
2025-07-27 17:47:51 -0400

    Interfaces de facturacion e inventario

--- aplicacion/blueprints/facturacion/routes.py
+++ aplicacion/blueprints/facturacion/routes.py
@@ -4,3 +4,13 @@ from flask_bcrypt import Bcrypt, generate_password_hash
 
 facturacion = Blueprint('facturacion', __name__, template_folder='templates', static_folder="static")
 bcrypt = Bcrypt()
+
+@facturacion.route('/',methods = ['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        return render_template('facturacion/index.html')
+
+@facturacion.route('/inventario',methods = ['GET', 'POST'])
+def inventario():
+    if request.method == 'GET':
+        return render_template('facturacion/inventario.html')
