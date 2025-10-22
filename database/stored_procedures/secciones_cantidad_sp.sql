5f1b820612d76565f1a805322c777dd3393380b8
Juan Koch <juanandreskochp@gmail.com>
2025-10-22 18:26:17 -0400

    Creacion de sp para obtener reportes de cuantas secciones hay mayor, menor o igual a cierta cantidad

---
+++ database/stored_procedures/secciones_cantidad_sp.sql
@@ -0,0 +1,38 @@
+ALTER PROCEDURE `secciones_cantidad_sp`(
+	IN id_curso INT,
+    IN cantidad INT,
+    IN mayorMenor TINYINT
+)
+BEGIN
+	IF mayormenor IS NULL THEN
+		SELECT sc.* 
+		FROM secciones sc 
+		WHERE sc.idCurso = id_curso
+            AND (
+                SELECT COUNT(*) 
+                FROM insc_x_seccion isc 
+                INNER JOIN inscripcion i ON isc.idInscripcion = i.idInscripcion 
+                WHERE isc.idSeccion = sc.idSeccion
+            ) = cantidad;
+	ELSEIF mayorMenor IS TRUE THEN
+		SELECT sc.* 
+		FROM secciones sc 
+		WHERE sc.idCurso = id_curso
+            AND (
+                SELECT COUNT(*) 
+                FROM insc_x_seccion isc 
+                INNER JOIN inscripcion i ON isc.idInscripcion = i.idInscripcion 
+                WHERE isc.idSeccion = sc.idSeccion
+            ) > cantidad;
+	ELSE
+		SELECT sc.* 
+		FROM secciones sc 
+		WHERE sc.idCurso = id_curso
+            AND (
+            SELECT COUNT(*) 
+            FROM insc_x_seccion isc 
+            INNER JOIN inscripcion i ON isc.idInscripcion = i.idInscripcion 
+            WHERE isc.idSeccion = sc.idSeccion
+            ) < cantidad;
+    END IF;
+END
